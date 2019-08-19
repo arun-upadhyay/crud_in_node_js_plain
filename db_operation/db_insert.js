9c89b3619adcbd7a7308001390dd17283790f2be
Arun Upadhyay <arunnnet@gmail.com>
2019-08-18 21:36:20 -0400

    url link

--- db_operation/db_insert.js
+++ db_operation/db_insert.js
@@ -1,6 +1,7 @@
 /**
  * Run your application as (to support export, by default node does not support es6 import)
  * node -r esm db_insert.js
+ * https://timonweb.com/tutorials/how-to-enable-ecmascript-6-imports-in-nodejs/
  */
 import {dbConnection} from "../conn/connection";
 
